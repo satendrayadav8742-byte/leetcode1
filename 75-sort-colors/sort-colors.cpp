768a2f8fa49d70e12ba1be246d2f3a086af2d420
Satendra Yadav <satendrayadav8742@gmail.com>
2026-07-26 00:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.89%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        int countR=0;
-        int countW=0;
-        int countB=0;
+       /* int n = nums.size();
+        int countR = 0, countW = 0, countB = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 0)
+                countR++;
+            else if(nums[i] == 1)
+                countW++;
+            else
+                countB++;
+        }
+
+        for(int i = 0; i < countR; i++)
+            nums[i] = 0;
+
+        for(int i = countR; i < countR + countW; i++)
+            nums[i] = 1;
+
+        for(int i = countR + countW; i < n; i++)
+            nums[i] = 2;*/
+        int n = nums.size();
+        int index=0;
         for(int i=0;i<n;i++)
         {
             if(nums[i]==0)
-            countR++;
-            else if(nums[i]==1)
-            countW++;
-            else
-            countB++;
-        }
-        for(int  i= 0;i<countR;i++)
+            {
+                swap(nums[i],nums[index]);
+                index++;
+            }
+
+        }   
+        for(int i=index;i<n;i++)
         {
-            nums[i]=0;
-        }
-        for(int i=countR;i<countR+countW;i++)
-        {
-            nums[i]=1;
-        }
-        for(int i=countR+countW;i<n;i++)
-        {
-            nums[i]=2;
-        }
-    
-        
+            if(nums[i]==1)
+            {
+                swap(nums[i],nums[index]);
+                index++;
+            }
+        } 
     }
+    
+
+
 };
