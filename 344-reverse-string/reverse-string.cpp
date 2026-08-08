6c924aca1f3f4fcc1b17cc3297dd189ef7d4ab47
Satendra Yadav <satendrayadav8742@gmail.com>
2026-08-08 10:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (98.76%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -2,18 +2,28 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         //first method by pointer
-        //second method by stack
-        stack<char>st;
-        for(int i=0;i<s.size();i++)
+        // //second method by stack
+        // stack<char>st;
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     st.push(s[i]);
+        // }
+        // int i=0;
+        // while(!st.empty())
+        // {
+        //     s[i]=st.top();
+        //     i++;
+        //     st.pop();
+       // }
+        //in this code take sc is o(n)so we use two pointer approach
+        int n= s.size();
+        int start = 0;
+        int end = n-1 ;
+        while(start<end)
         {
-            st.push(s[i]);
-        }
-        int i=0;
-        while(!st.empty())
-        {
-            s[i]=st.top();
-            i++;
-            st.pop();
+            swap(s[start],s[end]);
+            start++;
+            end--;
         }
     }
 };
