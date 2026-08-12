517e6dbf50dd62cdc7f8b8e11fdc4a6ad2a29d3d
Satendra Yadav <satendrayadav8742@gmail.com>
2026-08-13 00:16:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (93.16%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber){
+        string ans;
+        while(columnNumber)
+        {
+            columnNumber--;
+            int rem=columnNumber%26;
+            ans+='A'+rem;
+            columnNumber/=26;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    
+    }
+};          
