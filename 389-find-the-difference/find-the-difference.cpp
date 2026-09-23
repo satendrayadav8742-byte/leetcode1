81a79226c73326c19a0ff6ea477a4dcb66b2b73b
Satendra Yadav <satendrayadav8742@gmail.com>
2026-09-23 19:21:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (50.19%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans=0;
+        for(char c:s)
+        {
+            ans^=c;
+        }
+         for(char c:t)
+        {
+            ans^=c;
+        }
+        return ans;
+    }
+    
+};
